3c70ce73b061ba92687b56d718ef83baf3b0150e
kunsang <tenkus47@gmail.com>
2023-10-22 22:39:06 +0530

    fixed deploy

--- remix.config.js
+++ remix.config.js
@@ -1,17 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  cacheDirectory: "./node_modules/.cache/remix",
+export default {
   ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
   // appDirectory: "app",
   serverModuleFormat: "esm",
   serverDependenciesToBundle: ["@uidotdev/usehooks"],
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
+  server: "server.js",
   tailwind: true,
 };
