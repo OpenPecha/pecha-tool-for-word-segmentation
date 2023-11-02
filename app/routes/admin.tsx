eeed67de74b55cf0931bfdd364b19d1614636c1d
kunsang <tenkus47@gmail.com>
2023-11-02 22:46:44 +0530

    fix sidebar backside

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -25,9 +25,9 @@ const DefaultLayout = () => {
   return (
     <div className="dark:bg-boxdark-2 dark:text-bodydark">
       <div className="flex h-screen overflow-hidden">
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           <main className="pointer-events-auto">
             <div
               className={`mx-auto max-w-screen-2xl  ${
